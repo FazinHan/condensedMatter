f99dd8591cdf0778f49e202fec76b80d7bfc366a
FazinHan <freak.fizaan@gmail.com>
2024-08-13 19:05:11 +0530

    update

--- output_data/to_scratch.py
+++ output_data/to_scratch.py
@@ -1,9 +1,7 @@
 import os
 
-scratch_path = os.path.join('$SCRATCH','condensedMatter')
-
 for roots, dirs, fnames in os.walk('.'):
     for direc in dirs:
         if 'results_version' in direc:
             path = os.path.join(roots, direc)
-            os.system(f'mv {path} {scratch_path}')
+            os.system(f'mv {path} $SCRATCH/condensedMatter')
