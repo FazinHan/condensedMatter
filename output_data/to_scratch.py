f2265694963a1812023e35426428099e239bcb0d
FazinHan <freak.fizaan@gmail.com>
2024-08-13 19:02:05 +0530

    update

--- output_data/to_scratch.py
+++ output_data/to_scratch.py
@@ -5,4 +5,5 @@ scratch_path = os.path.join('$SCRATCH','condensedMatter')
 for roots, dirs, fnames in os.walk('.'):
     for direc in dirs:
         if 'results_version' in direc:
-            os.rename(os.path.join(roots, direc), scratch_path)
+            path = os.path.join(roots, direc)
+            os.system(f'mv {path} {scratch_path}')
