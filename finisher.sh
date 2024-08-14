35736822bf52675bd5133ee10c6015c121482248
fizaank.phy21.iitbhu <fizaan.khan.phy21@iitbhu.ac.in>
2024-08-14 14:36:43 +0530

    update

--- finisher.sh
+++ finisher.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#SBATCH -A physics_engg
+#SBATCH --ntasks=1
+#SBATCH --job-name=clean.up
+#SBATCH --output=output_run1/cleanup.out
+
+echo "========= Job started  at `date` on `hostname -s` =========="
 
 cd output_data
 
@@ -19,3 +25,6 @@ python to_scratch.py
 echo "data sent to scratch"
 
 git add . && git commit -m "update" && git push
+
+echo "========= Job finished at `date` =========="
+
