10b144aab6ade58a531fecfec8fbdaee1d7ec273
FazinHan <freak.fizaan@gmail.com>
2024-08-15 17:16:42 +0530

    code cleanup

--- plotter.py
+++ plotter.py
@@ -6,20 +6,13 @@ import matplotlib.pyplot as plt
 
 def plotter(L, conductivities, beta, save, name='output', folder=''):
     fig, axs = plt.subplots(2,1)
-    # t2 = time.perf_counter()
-    # print(f'parallelised: {np.round(t2-t1,3)}s taken')
     
     axs[1].plot(L, conductivities,'.')
     axs[0].plot(conductivities, beta,'.')
-    # axs[0].set_xlabel('$\\eta$')
     axs[1].set_xlabel('L')
     axs[1].set_ylabel('g')
     axs[0].set_xlabel('g')
     axs[0].set_ylabel('$\\beta$')
-    # if name=='output':    
-    #     fig.suptitle(randomness.__name__[:-8])
-    # else:
-    #     fig.suptitle(name)
     fig.tight_layout()
     name = determine_next_filename('plot', folder=folder, filetype='png')#fname='eta_variance')
     if save:
@@ -38,11 +31,9 @@ def main(save):
     directory = dirname
     
     for root, _, fnames in os.walk(directory):
-    # for root, _, fnames in os.walk(dirname):
         for fname in fnames:
             if 'params.txt' not in fname and 'txt' in fname:# and 'results_version' not in root:
                 with open(os.path.join(root, fname),'r') as file:
-                    # print(root,fname)
                     data = eval(file.read())
                     conductivities.append(data)
     conductivities = np.array(conductivities)
@@ -52,14 +43,9 @@ def main(save):
     with open(fname,'rb') as file:
         L = np.load(file)
 
-    # print(conductivities.shape)
-    # print(L.shape)
-
-    # print(L.shape)
     cs = CubicSpline(np.log(L), np.log(conductivities))
     beta = cs(np.log(L), 1)
     plotter(L, conductvities, beta, save, folder=dirname)
-    # plotter(L, np.abs(g), np.abs(beta), name!='output', name)
 
 if __name__=="__main__":
     try:
