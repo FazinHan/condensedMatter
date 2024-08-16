10a0c9d5c27d0b8b84912a64352ef134963b9718
FazinHan <freak.fizaan@gmail.com>
2024-08-16 23:24:07 +0530

    huge unbug

--- main.py
+++ main.py
@@ -182,17 +182,23 @@ if __name__ == "__main__":
     
     L = [np.linspace(l_min, l_max,3*5)] * configurations
 
+    print('debug 1')
+
     _ = [main(i) for i in L]
+
+    print('debug 2')
     
     dirname = os.path.join('output_data','results_version','run'+sys.argv[1])
     
     fname = determine_next_filename(fname='length',folder=dirname, filetype='npy')
     np.save(fname, L[0])
+    print('debug 3')
     if not os.path.isfile(os.path.join('output_data','results_version','params.txt')):
         with open(os.path.join('output_data','results_version','params.txt'),'w') as file:
             text = f'''l_min, l_max = {l_min}, {l_max}\nvf = {vf}\nh_cut = {h_cut}\nu = {u}\nl0 = {l0}\nN_i = {N_i}\neta = {eta}\nT = {T}\nef = {ef}\nconfigurations = {configurations}\nk_space_size = {k_space_size}\nscattering potential = {function}'''#\na = {a}'''
             file.write(text)
             print('parameter file written')
+    print('debug 4')
     
    
 if __name__=="__main__1":
