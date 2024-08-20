df8aa0a0408e7252e959ab3f00ef873e8208683c
fizaank.phy21.iitbhu <fizaan.khan.phy21@iitbhu.ac.in>
2024-08-20 18:02:07 +0530

    update

---
+++ non-destructive.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH -A physics_engg
+#SBATCH --ntasks=1
+#SBATCH --job-name=clean.up
+#SBATCH --output=output_run1/cleanup.out
+#SBATCH --array=1-5%5
+#SBATCH --ntasks=40
+
+module load conda
+module load git_2.41
+
+echo "========= Job started  at `date` on `hostname -s` =========="
+
+for num in $(seq 1 $ntasks)
+
+do
+srun --exclusive --ntasks=1 python plotter.py save ${runstring[*]}${num}
+done
+
+#mv --backup=t output_data/results_version $SCRATCH/data-condensedMatter
+
+#echo "data sent to scratch"
+
+#mkdir output_data/results_version
+
+echo "========= Job finished at `date` =========="
+
