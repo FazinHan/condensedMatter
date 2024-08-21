3516c3185c82f79120488c92125d7b422fa4225f
FazinHan <freak.fizaan@gmail.com>
2024-08-21 20:54:51 +0530

    update

--- timer.sh
+++ timer.sh
@@ -4,9 +4,15 @@
 #SBATCH --ntasks=5
 
 module load conda
-module load parallel
+module load intel/2018.0.1.163
+module unload gnu8/8.3.0
+source /opt/ohpc/pub/intel2018/compilers_and_libraries_2018.1.163/linux/mkl/bin/mklvars.sh intel64
+
+export I_MPI_FALLBACK=disable
+export I_MPI_FABRICS=shm:tmi
+export I_MPI_DEBUG=9
+export OMP_NUM_THREADS=40
 
 echo "===========FULL SYSTEM SIZE TIMER=========="
 
-parallel -j 5 python -m timeit "print('timing {}...)" "from main import {}" "{}()" ::: "get_k_space" "ft_potential_builder_3" "hamiltonian" "conductivity_vectorised" "determine_next_filename"
-
+echo $OMPI_COMM_WORLD_RANK
