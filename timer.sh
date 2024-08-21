1e51230a64fb288920307e62f363c536229fcbb1
FazinHan <freak.fizaan@gmail.com>
2024-08-21 13:07:25 +0530

    update

--- timer.sh
+++ timer.sh
@@ -7,21 +7,21 @@ module load conda
 echo "===========FULL SYSTEM SIZE TIMER=========="
 
 echo "Timing get_k_space..."
-python -m timeit "from main import *" "get_k_space()"
+python -m timeit "from main import get_k_space" "get_k_space()"
 echo ""
 
 echo "Timing ft_potential_builder_3..."
-python -m timeit "from main import *" "ft_potential_builder_3()"
+python -m timeit "from main import ft_potential_builder_3" "ft_potential_builder_3()"
 echo ""
 
 echo "Timing hamiltonian..."
-python -m timeit "from main import *" "hamiltonian()"
+python -m timeit "from main import hamiltonian" "hamiltonian()"
 echo ""
 
 echo "Timing conductivity_vectorised..."
-python -m timeit "from main import *" "conductivity_vectorised()"
+python -m timeit "from main import conductivity_vectorised" "conductivity_vectorised()"
 echo ""
 
 echo "Timing determine_next_filename..."
-python -m timeit "from main import *" "determine_next_filename()"
+python -m timeit "from main import determine_next_filename" "determine_next_filename()"
 echo ""
