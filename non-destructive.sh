cee8480a6fcb77243f0dfd15be762d5fec3ef9d2
FazinHan <freak.fizaan@gmail.com>
2024-08-25 11:49:59 +0530

    update

--- non-destructive.sh
+++ non-destructive.sh
@@ -3,26 +3,11 @@
 #SBATCH --ntasks=1
 #SBATCH --job-name=clean.up
 #SBATCH --output=output_run1/cleanup.out
-#SBATCH --array=1-5
-#SBATCH --ntasks=40
 
-module load conda
-
-export I_MPI_FALLBACK=disable
-export I_MPI_FABRICS=shm:tmi
-export I_MPI_DEBUG=9
-export OMP_NUM_THREADS=40
-
-runstring=(`echo ${SLURM_ARRAY_TASK_ID:-1}`)
 
 echo "========= Job started  at `date` on `hostname -s` =========="
-ntasks=$SLURM_NTASKS
 
-for num in $(seq 1 $ntasks)
-
-do
-srun --exclusive --ntasks=1 python condensor.py
-done
+python condensor.py
 
 echo "========= Job finished at `date` =========="
 
