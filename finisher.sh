2b5ffda42c7a2d9608c8ca093d748e8744412af4
fizaank.phy21.iitbhu <fizaan.khan.phy21@iitbhu.ac.in>
2024-08-25 17:25:20 +0530

    update

--- finisher.sh
+++ finisher.sh
@@ -6,16 +6,16 @@
 #SBATCH --array=1-5%5
 #SBATCH --ntasks=40
 
-module load conda
-module load git_2.41
+#module load conda
+#module load git_2.41
 
 echo "========= Job started  at `date` on `hostname -s` =========="
 
-for num in $(seq 1 $ntasks)
+#for num in $(seq 1 $ntasks)
 
-do
-srun --exclusive --ntasks=1 python plotter.py save ${runstring[*]}${num}
-done
+#do
+#srun --exclusive --ntasks=1 python plotter.py save ${runstring[*]}${num}
+#done
 
 mv --backup=t output_data/results_version $SCRATCH/data-condensedMatter
 
