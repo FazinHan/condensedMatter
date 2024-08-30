48b1c0a5d601a6c5d20bab7641c4425adb560789
FazinHan <freak.fizaan@gmail.com>
2024-08-30 09:38:35 +0530

    Refactor tests.py to add test_by_points function

--- tests.py
+++ tests.py
@@ -65,8 +65,38 @@ def test_conductivity_vectorised_real_output(L=10):
     time_taken = np.round(end_time - start_time, 3)
     print(f"Conductivity tests passed\nTime taken: {time_taken} seconds")
 
+def test_by_points():
+    
+    lamda = 20*np.pi/L
+    k_vec = np.linspace(-lamda, lamda, k_space_size) # N
+    cartesian_product = np.array(np.meshgrid(k_vec, k_vec, indexing='ij')).T.reshape(-1, 2)
+    
+    def potential(kx, ky):
+        gaus = u * np.exp(-(kx**2+ky**2)*l0**2/2)
+        R_i = rng.uniform(low=-L/2,high=L/2,size=(2,N_i*L**2))
+        k_matrix = 0
+        for i in range(N_i*int(L)**2):
+            rands1 = R_i[0,i]
+            rands2 = R_i[1,i]
+            k_matrix += np.exp(1j * (kx * rands1 + ky * rands2)) * gaus
+        return k_matrix/L**2
+
+
+    def hamiltonian(kx, ky):
+        sdotk = kx*sx+ky*sy
+        v_q = potential(kx,ky)
+        return 1*sdotk + v_q
+    
+    for i, j in cartesian_product:
+        h = hamiltonian(i, j)
+        if np.allclose(1j*sy @ h.conj().T @ (-1j*sy), h):
+            print(f'yes; h=\n{h}\nkx=\n{i}\nky=\n{j}')
+    else:
+        print('>>> k-points exhausted\n')
+
 if __name__ == '__main__':
     test_randomiser()
     test_k_space()
     test_conductivity_vectorised_real_output()
+    test_by_points()
     test_hamiltonian()
