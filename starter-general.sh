2ecdf47ac8060f44906faaea7a2c1a60da6a6fd5
FazinHan <freak.fizaan@gmail.com>
2024-10-14 12:42:20 +0530

    Refactor starter-general.sh: Update number of tasks per node and number of threads

--- starter-general.sh
+++ starter-general.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=tilted.fermion
 #SBATCH --output=fermion.out
 #SBATCH --time=4-00:00:00
-#SBATCH --ntasks-per-node=40
+#SBATCH --ntasks-per-node=24
 
 echo "========= Job started  at `date` on `hostname -s` =========="
 
@@ -15,14 +15,13 @@ echo "Job id       : $SLURM_JOB_ID"
 export I_MPI_FALLBACK=disable
 export I_MPI_FABRICS=shm:tmi
 export I_MPI_DEBUG=9 
-export OMP_NUM_THREADS=40
+export OMP_NUM_THREADS=24
 
 source ~/.bashrc
 
 module load julia
 
-time mpiexec.hydra -genv I_MPI_DEBUG 9 -n $SLURM_NTASKS -genv OMP_NUM_THREADS 40 julia julia-main2.jl
-# time mpiexec.hydra -genv I_MPI_DEBUG 9 -n $SLURM_NTASKS -genv OMP_NUM_THREADS 40 python main.py
+time mpiexec.hydra -genv I_MPI_DEBUG 9 -n $SLURM_NTASKS -genv OMP_NUM_THREADS 24 julia julia-main2.jl
 
 echo "======== Conductivities finished at `date` ========="
 
